0a0863c1d3b383fec08952217c47d0e11c2de1ae
Jason Yang <jasonyangmh@gmail.com>
2022-06-30 01:51:19 +0700

    Generate status scaffold

---
+++ db/migrate/20220629185041_create_statuses.rb
@@ -0,0 +1,15 @@
+class CreateStatuses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :statuses do |t|
+      t.string :type
+      t.string :name
+      t.string :position
+      t.text :address
+      t.date :start_date
+      t.date :end_date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
