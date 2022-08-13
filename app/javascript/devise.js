bdbf19d494142aeeb77ba43f2eae269d5eb79cf3
Jason Yang <jasonyangmh@gmail.com>
2022-08-13 11:01:28 +0700

    Move the stylesheet and javascript for devise to their own files

---
+++ app/javascript/devise.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+//= jquery.min.js
+//= bootstrap.bundle.min.js
+//= jquery.easing.min.js
+//= sb-admin-2.min.js
