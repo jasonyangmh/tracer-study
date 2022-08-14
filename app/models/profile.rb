244b6cd84a10117cf519f7c1cb458704191ca389
Jason Yang <jasonyangmh@gmail.com>
2022-08-14 19:33:05 +0700

    Add validation to all fields in profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :birthplace, presence: true
+  validates :birthdate, presence: true
+  validates :gender, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :major, presence: true
+  validates :graduation_year, presence: true
 end
