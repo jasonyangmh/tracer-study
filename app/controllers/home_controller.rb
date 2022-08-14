2a8e03c1744121f0020ee64b27e1bb7f993dda30
Jason Yang <jasonyangmh@gmail.com>
2022-08-15 05:24:48 +0700

    Redirect user to new profile if user doesn't have one in root path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,15 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to statuses_path
+      if current_user.profile
+        redirect_to statuses_url
+      else
+        redirect_to new_profile_url
+      end
     end
 
     unless user_signed_in? or admin_signed_in?
-      redirect_to new_user_session_path
+      redirect_to new_user_session_url
     end
   end
 end
